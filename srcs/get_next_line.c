b8866ca44cdff92ca29a75eed62c4ec5a0cdfde7
Gabor Horvath-Ulenius <ghorvath@student.hive.fi>
2021-12-14 11:17:44 +0200

    testing phase

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ghorvath <ghorvath@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 12:26:10 by ghorvath          #+#    #+#             */
-/*   Updated: 2021/12/13 14:32:10 by ghorvath         ###   ########.fr       */
+/*   Updated: 2021/12/14 10:43:27 by ghorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,6 @@ int	get_next_line(const int fd, char **line)
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	;
 	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[ret] = '\0';
